1c9da4ec532a9734c255d8b9d17851511c7114f3
Haira Lei <darhaii13@gmail.com>
2022-08-08 21:57:29 +0800

    Add and style error, about, and checkout page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='About' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='Nice desk' />
+        <article>
+          <div className='title'>
+            <h2>Our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat
+            optio, odit reprehenderit obcaecati dolorem dolore quod in sapiente
+            vel eaque. Eaque qui libero dignissimos. Molestiae possimus officia
+            excepturi saepe quod dolorem et ea natus eum exercitationem? Ratione
+            tempora ullam officiis obcaecati possimus? Modi officiis dolores eum
+            ab rem optio facilis eius culpa explicabo voluptates commodi vel
+            deleniti molestias consequuntur quibusdam quaerat repudiandae facere
+            voluptate quas unde blanditiis necessitatibus, dolorum, delectus
+            est. Consectetur quae placeat commodi quidem nemo, dolorum explicabo
+            exercitationem.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +58,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
