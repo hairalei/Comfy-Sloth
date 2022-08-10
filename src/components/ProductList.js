22df3dab4d8400111ce5643ad8e40cd798506d7d
Haira Lei <darhaii13@gmail.com>
2022-08-10 16:37:20 +0800

    Add sort by list and sort by grid components and functionality

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React from 'react';
+import { useFilterContext } from '../context/filter_context';
+import products_reducer from '../reducers/products_reducer';
+import GridView from './GridView';
+import ListView from './ListView';
 
 const ProductList = () => {
-  return <h4>product list</h4>
-}
+  const { filteredProducts, gridView } = useFilterContext();
 
-export default ProductList
+  if (filteredProducts.length < 1) {
+    return (
+      <h4 style={{ textTransform: 'none' }}>
+        Sorry, no products matched your search...
+      </h4>
+    );
+  }
+
+  if (!gridView) {
+    return <ListView filteredProducts={filteredProducts} />;
+  }
+
+  return <GridView filteredProducts={filteredProducts}>product list</GridView>;
+};
+
+export default ProductList;
