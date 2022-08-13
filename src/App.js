62f608040f37bc4a8d6fdabe562c710f74606ac2
Haira Lei <darhaii13@gmail.com>
2022-08-13 15:47:38 +0800

    Setup private route and fix bug using AuthWrapper page

--- src/App.js
+++ src/App.js
@@ -15,29 +15,31 @@ import {
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Sidebar />
+    <AuthWrapper>
+      <Router>
+        <Navbar />
+        <Sidebar />
 
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/cart' element={<CartPage />} />
-        <Route path='/products' element={<ProductsPage />} />
-        <Route path='/products/:id' element={<SingleProductPage />} />
-        <Route
-          path='/checkout'
-          element={
-            <PrivateRoute>
-              <CheckoutPage />
-            </PrivateRoute>
-          }
-        />
-        <Route path='*' element={<ErrorPage />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/cart' element={<CartPage />} />
+          <Route path='/products' element={<ProductsPage />} />
+          <Route path='/products/:id' element={<SingleProductPage />} />
+          <Route
+            path='/checkout'
+            element={
+              <PrivateRoute>
+                <CheckoutPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
 
-      <Footer />
-    </Router>
+        <Footer />
+      </Router>
+    </AuthWrapper>
   );
 }
 
